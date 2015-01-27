17db6463d1a91c66d096692c504646ed35974b3d
Remus Lazar <remus@lazar.info>
2015-01-27 20:31:48 +0100

    playing with TIMER3

--- Arduino/LedTemperatureDisplay/LedTemperatureDisplay.ino
+++ Arduino/LedTemperatureDisplay/LedTemperatureDisplay.ino
@@ -7,16 +7,6 @@ void setup() {
 
 #define DEBUG
 
-#ifdef DEBUG
-	Serial.begin(9600);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for Leonardo only
-	}
-
-	// prints title with ending line break
-	Serial.println(F("ready()"));
-#endif
-
 	// setup our LCDDisplay instance
 	display.begin(true);
 	display.setTextColor(LED_ORANGE_COLOR);
@@ -33,18 +23,56 @@ void setup() {
 	//	display.clearScreen();
 	// display.fillScreen(LED_ORANGE_COLOR);
 	//display.setBrightness(80);
+
+#ifdef DEBUG
+	Serial.begin(9600); // but don't block!
+#endif
+
+
+#define F_IDLE_LOOP 1
+#define PWM_LED .1
+
+#define TIMER_R F_CPU * (1+PWM_LED) / 1024 / F_IDLE_LOOP
+#define CNT_PWM (1.0-PWM_LED) * TIMER_R
+
+	TCCR3A  = _BV(WGM31) ; // fast PWM, mode 14
+	TCCR3B  = _BV(WGM33) | _BV(WGM32) | _BV(CS30) | _BV(CS32); // clk/1024 prescaler
+	ICR3 = TIMER_R;
+
+	TIMSK3 |= _BV(TOIE3); // enable irq for timer1
+	sei();                // enable global irq
 }
 
-void loop() {
+ISR(TIMER3_OVF_vect, ISR_BLOCK) {
+	volatile static bool led = false;
+
+	TCNT3 = led ? CNT_PWM : 0;
+	digitalWrite(LED_BUILTIN, led);
+	if (!(led = !led)) {
+		// do stuff F_IDLE_LOOP Hz
 #ifdef DEBUG
-	static uint32_t lastCountTime = 0;
-#define DISPLAY_REFRESH 1
-	//display.updateDisplay();
-	if ((millis() - lastCountTime) > DISPLAY_REFRESH * 1000) {
-		lastCountTime = millis();
-		Serial.print("refresh/s: ");
-		Serial.println(display.refresh/DISPLAY_REFRESH);
-		display.refresh = 0;
-	}
+		showLedRefreshRate();
 #endif
+	}
+
+	TIFR3 |= TOV3;
+}
+
+#ifdef DEBUG
+void showLedRefreshRate() {
+	static uint32_t lastTimestamp = 0;
+	uint32_t now = millis();
+	double timeElapsed = (now - lastTimestamp) / 1000.0;
+
+	if (Serial) { // if Serial available, print out the refresh rate
+		Serial.print("refresh/s: ");
+		Serial.println(display.refresh/timeElapsed);
+	}
+	display.refresh = 0;
+	lastTimestamp = now;
+}
+#endif
+
+void loop() {
+
 }
